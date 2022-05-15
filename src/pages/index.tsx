bcace01fc429721c67fffa435d5e256a96daf8a5
Masahiko Jinno <mjinno@mbp14.lan>
2022-05-15 23:21:47 +0900

    fix: csr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
 
+import { useCSR } from '../components/hooks'
 import { Home } from '../components/pages/Home'
 
 const Page: NextPage = () => {
+  const execRouting = useCSR()
+  if (!execRouting) {
+    // ToDo: 必要あればローディング中のUIを表示する
+    return null
+  }
   return <Home />
 }
 
