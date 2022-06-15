bf66e57e2c9e41e155120ddc216d5977e9efe71f
kunori <ykunori1986@gmail.com>
2022-06-15 22:44:48 +0900

    fix

--- .storybook/storyshots/storybook.test.ts
+++
@@ -1,7 +0,0 @@
-import initStoryshots, { multiSnapshotWithOptions } from '@storybook/addon-storyshots'
-import path from 'path'
-
-initStoryshots({
-  configPath: path.resolve(__dirname, './main.js'),
-  test: multiSnapshotWithOptions()
-})
