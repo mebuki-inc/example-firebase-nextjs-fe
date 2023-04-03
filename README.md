abf36429a427ed2d6a46340496d45c3883c01546
JIN <30728911+JinTakenaka@users.noreply.github.com>
2023-04-03 12:42:56 +0900

    add comments

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+## 概要
 Firebase x Next.js App
+
+## 備考
+### storyShotsについて
+mswでのstoryshotsを行うため少々ハック気味の実装を行っています。
+- 各handlers.ts にて下記方法でloading状態を実現 <br>通常何かしらの値を返すべきところ、何も返さないことでloadingの挙動を偽装しています
+- storybook.test.ts にて再描画を待ってからsnapshotを行う<br>DOMはloadingの後にsuccess/errorをrenderするので、2回目のrenderが終わってからsnapshotを行っています
--- src/pages/My/handers/fetchMy.handlers.ts
+++ src/pages/My/handers/fetchMy.handlers.ts
@@ -8,6 +8,7 @@ export const handlers = {
   default: rest.get(`${apiHost}/v1/user/self`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(samples['200']))
   }),
+  //　通常返却されるべき値を返却しないことでloading状態を偽装する
   loading: rest.get(`${apiHost}/v1/user/self`, (req, res, ctx) => new Promise(() => undefined)),
   error: rest.get(`${apiHost}/v1/user/self`, (req, res, ctx) => {
     return res(ctx.status(500), ctx.json({ errorMessage: 'internal server error' }))
